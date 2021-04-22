84b9cdbcd8e7f64d90280a48771fb4c633c8c9aa
eric <ericgip97@gmail.com>
2021-04-22 16:31:01 -0700

    finished configuring and deploying

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.listen(port);
 console.log(`Server running at http://localhost: ${port}`);
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'guest';
-  res.send(`Hello ${name}!`);
+  const name = process.env.NAME || 'Proof of Modification by Eric Gip';
+  res.send(`Hello ${name}! Proof of Modification by Eric Gip`);
 });
